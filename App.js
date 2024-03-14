a80602e8c4a80219d9809a7f3296fc235042b309
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2024-03-14 23:47:58 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/*
+<div>
+    <div>
+        <h1></h1>
+*/
+
+// Creating nested elements using react without JSX
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+    React.createElement("div", {id: "child2"}, [
+            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading", xyz: "abc" },
+    "Hello World from React"
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
