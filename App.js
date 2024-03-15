e1b96c4173d9f590018fa846cd39d5b594be460e
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2024-03-15 19:33:21 +0530

    Episode 02

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-/*
-<div>
-    <div>
-        <h1></h1>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// Creating nested elements using react without JSX
 const parent = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child"}, [
-            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h1", {}, "I'm shreyansh"),
             React.createElement("h2", {}, "I'm an h2 tag")
     ]),
     React.createElement("div", {id: "child2"}, [
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React 
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- Uses a File Watching Algorithm written in C++
+- Gives you Faster Builds using Caching
+- Image Optimization
+- Minification of files
+- Bundling of files
+- Compress files
+- Consistent Hashing
+- Code Splitting
+- Differential Bungling (Generate fallback for older browsers)
+- Error Handling (Beautiful Diagnostics)
+- HTTPs
+- Tree Shaking (Remove unused code)
+- Different dev and prod bundles
