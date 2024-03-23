0a014283b3559cf3e1b20092d1bc078e77882fbc
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2024-03-23 20:51:07 +0530

    Impletmented API call and Search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,57 @@
 import RestaurantCard from "./RestaurantCard";
-import {useState} from "react";
-import resList from "../utils/mockData"
+import {useState, useEffect} from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  console.log("Body Renderd");
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  return (
+  const fetchData = async () => {
+    const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const data = await fetch(API_URL);
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  
+
+  return listOfRestaurants.length === 0 ? ( 
+    <Shimmer/>
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input 
+            type="text" 
+            className="search-box" 
+            value={searchText} 
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button 
+            onClick={() => {
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter(
+                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button 
           className="filter-btn" 
           onClick={() => {
@@ -22,7 +65,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>
         ))}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -16,9 +16,9 @@ const RestaurantCard = (props) => {
         <img className="res-logo" alt="res-logo" src={ CDN_URL + cloudinaryImageId }/>
         <h3>{name}</h3>
         <h4>{cuisines.join(", ")}</h4>
-        <h4>{avgRating}</h4>
+        <h4>{avgRating}⭐</h4>
         <h4>{costForTwo}</h4>
-        <h4>{sla.deliveryTime} min</h4>
+        <h4>Delivery in {sla.deliveryTime} min</h4>
       </div>
     );
   };
