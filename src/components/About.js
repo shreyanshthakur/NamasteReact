8af4d7f4cfb2d934b8bbd459c2af69326e7eb948
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2024-04-02 00:15:02 +0530

    Solved props drilling problem using React context

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,32 @@
 import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
-
+import UserContext from "../utils/UserContext";
 
 class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-    
-    componentDidMount() {
-        
-    }
+  componentDidMount() {}
 
-    render() {
-        
-        return (
-            <div>
-                <h1>About</h1>
-                <h2>This is about component</h2>
-                <UserClass name="Shreyansh (Class)"/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <div>
+          Current User:
+          <UserContext.Consumer>
+            {({ loggedInUser }) => (
+              <h1 className="text-xl font-bold">{loggedInUser}</h1>
+            )}
+          </UserContext.Consumer>
+        </div>
+        <h2>This is about component</h2>
+        <UserClass name="Shreyansh (Class)" />
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,24 @@
-import { Link } from "react-router-dom";
-import {LOGO_URL} from "../utils/constants";
-import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
 
 const Header = () => {
-
   const [btnNameReact, setBtnNameReact] = useState("Login");
 
   const onlineStatus = useOnlineStatus();
 
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <div className="flex justify-between  shadow-md h-100">
       <div className="h-100">
-        <img
-          className="w-[110px]"
-          src={LOGO_URL}
-        />
+        <img className="w-[110px]" src={LOGO_URL} />
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li className="px-4">
-            Online Status: {onlineStatus ? "✅" : "🔴"}
-          </li>
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
           <li className="px-4">
             <Link to="/">Home</Link>
           </li>
@@ -36,18 +32,21 @@ const Header = () => {
             <Link to="/grocery">Grocery</Link>
           </li>
           <li className="px-4">Cart</li>
-          <button 
-            className="login" 
+          <button
+            className="login"
             onClick={() => {
-              btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
             }}
           >
             {btnNameReact}
           </button>
+          <li className="px-4 font-bold">{loggedInUser}</li>
         </ul>
       </div>
     </div>
   );
 };
 
-  export default Header;
+export default Header;
